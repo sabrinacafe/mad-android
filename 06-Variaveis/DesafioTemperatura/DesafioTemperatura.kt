11ad7eaf86c4bb34185285987ac4fda33d952fc5
Sabrina <sabis476@gmail.com>
2024-08-14 20:50:59 -0300

    [Desafio Temperatura]

---
+++ 06-Variaveis/DesafioTemperatura/DesafioTemperatura.kt
@@ -0,0 +1,9 @@
+fun main(){
+
+    // Declarando as variáveis 
+    val celsius = 25.0
+    val fahrenheit = celsius * 9/5 + 32
+    val kelvin = celsius + 273.15
+
+    println("A temperatura em Celsius é: $celsius, portanto, fahrenheit $fahrenheit e kelvin $kelvin ")
+}
