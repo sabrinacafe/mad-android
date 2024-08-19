f2cd5867845f67845f91ef6d460cfdf51c100080
Sabrina <sabis476@gmail.com>
2024-08-19 19:19:44 -0300

    [Ex. Array]

---
+++ 07-Array/Exercicios/Array.kt
@@ -0,0 +1,11 @@
+fun main (){
+    // Vamos supor que temos o seguinte array de numeros
+    val numeros = arrayOf(10,20,30,40,50)
+
+    // vamos usar um loop para imprimir cada número do array
+    for(numero in numeros){
+    println("Número: $numero")
+}
+
+}
+
