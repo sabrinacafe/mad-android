0eee953fb2461ea99a717429ad231a266adeb464
Sabrina <sabis476@gmail.com>
2024-08-21 19:58:14 -0300

    [Classificacao numero]

---
+++ 08-Condicional/IF/Classificacao-numerica.kt
@@ -0,0 +1,16 @@
+fun main(){
+
+    val numero = 7 // Definindo o valor do numero a ser verificado
+
+    if (numero > 0){// Verifica se o numero é maior que zero
+
+    println("O numero é positivo") // Ser verdadeiro vai imprimir isso aqui
+
+    } else if (numero < 0){//Caso contrario varifica se o numero é menor que zero
+
+    println("O numero é negativo") //se verdadeiro imprime esse mensagem
+
+    } else {
+        println("O numero é zero") //se nenhuma das alternativas for verdadeira
+    }
+}
