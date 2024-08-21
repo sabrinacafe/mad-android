7e9c6e876619a3633d57b5979c3c315301521bf4
Sabrina <sabis476@gmail.com>
2024-08-21 04:49:41 -0300

    [Desafio ImparPar]

--- 07-Array/Desafio/Soma-de-Array.kt
+++ 07-Array/Desafio/Soma-de-Array.kt
@@ -1,6 +1,18 @@
-fun main(){
+fun main() {
+    // Arrays com os números pares e ímpares
+    val pares = arrayOf(0, 2, 4, 6)
+    val impares = arrayOf(1, 3, 5, 7)
 
-    var pares = (0, 2, 4,6)
-    var impares = (1, 3, 5, 7)
-    
+    // Array para armazenar as somas
+    val somas = Array(pares.size) { 0 }
+
+    // Realizando a soma dos elementos correspondentes
+    for (i in pares.indices) {
+        somas[i] = pares[i] + impares[i]
+    }
+
+    // Exibindo os resultados das somas
+    for (i in somas.indices) {
+        println("A soma de ${pares[i]} e ${impares[i]} é ${somas[i]}")
+    }
 }
