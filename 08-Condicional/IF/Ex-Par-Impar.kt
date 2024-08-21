178ba20eef4798095ad379ea892b7ddeb7f59dad
Sabrina <sabis476@gmail.com>
2024-08-21 20:15:05 -0300

    [Exercicios]

---
+++ 08-Condicional/IF/Ex-Par-Impar.kt
@@ -0,0 +1,12 @@
+// Faça um programa que verifica se o numero é par ou impar 
+
+fun main(){
+
+    val numero = 32
+
+    if (numero % 2 == 0){
+        println("O número é par")
+    }else {
+        println("O número é impar")
+    }
+}
---
+++ 08-Condicional/IF/Ex-VerificaAcesso.kt
@@ -0,0 +1,13 @@
+// Crie um programa que verifica se o usuário tem acesso ao sistema ou não
+
+fun main(){
+
+    val login = "vinny"
+    val senha = "123456"
+
+    if (login == "vinny" && senha == "123456"){
+        println("Acesso permitido")
+    }else {
+        println("Acesso negado")
+    }
+}
